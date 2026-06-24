30ab33ba5bc75504585ea0e2114987c3927789a6
Chanon18820 <chanonwilaikum@gmail.com>
2026-06-24 10:39:27 +0700

    <py>Implement coffee-making simulation for customers in synchronous manner

--- Week1/pid01_synchronous.py
+++ Week1/pid01_synchronous.py
@@ -2,13 +2,43 @@ from time import sleep, ctime, time
 import os
 import threading
 
-# ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คนแบบซิงโครนัส
+# ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คนแบบทีละคน
 def make_coffee(customer_name):
-    pass
+
+    # ดึง PID และ Thread ID ออกมาดู
+    pid = os.getpid()
+    thread_id = threading.current_thread().native_id
+    thread_name = threading.current_thread().name
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] "
+          f"[Thread Name: {thread_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...")
+
+    sleep(5)  # จำลองการทำงานของ Thread นี้ 5 วินาที
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] "
+          f"[Thread Name: {thread_name}] ลูกค้า {customer_name}: ได้รับกาแฟแล้ว!")
+
 
 def main():
-    pass
+
+    queue = ['A', 'B', 'C']
+
+    main_pid = os.getpid()
+    main_tid = threading.current_thread().native_id
+
+    print(f"{ctime()} | [Main PID: {main_pid}] "
+          f"[Main TID: {main_tid}] === เริ่มระบบจำลองลูกค้าแบบ Synchronous ===")
+
+    start_time = time()
+
+    # ชงทำงานตามลำดับทีละคน (ทีละคิว)
+    for customer in queue:
+        make_coffee(customer)
+
+    duration = time() - start_time
+
+    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:0.2f} วินาที")
+
 
 if __name__ == "__main__":
     main()
-    
